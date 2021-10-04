ccb452ed7925b2d3a1010dab221edb592a7b6653
Duy <huykhaduy@gmail.com>
2021-10-04 11:21:24 +0700

    Change modul detail

--- libary/read_modul.cpp
+++ libary/read_modul.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 int loadDataFromTxtToArr(FILE *filein, int *&arr){
+    bool error_flag = 0;
     if (filein==NULL){
         std::cout << "Get error when open your file" << std::endl;
-        return 0;
+        error_flag = 1;
     }
     int n;
     if (fscanf(filein, "%d", &n)==EOF){
         std::cout<<"Get error when read your file"<<std::endl;
-        return 0;
+        error_flag = 1;
     }
     arr = new int[n];
     if (arr==NULL){
         std::cout<<"Get error when declare the array"<<std::endl;
+        error_flag = 1;
     }
+    if (error_flag==1)
+        return 0;
     int i = 0;
     while (!feof(filein)){
         fscanf(filein, "%d", arr + i++);
@@ -22,9 +26,10 @@ int loadDataFromTxtToArr(FILE *filein, int *&arr){
 }
 
 int loadDataFromTxtToArr_NoElement(FILE *filein, int *&arr){
+    bool error_flag = 0;
     if (filein==NULL){
         std::cout << "Get error when open your file" << std::endl;
-        return 0;
+        error_flag = 1;
     }
     int n=0;
     int tempValue;
@@ -34,7 +39,11 @@ int loadDataFromTxtToArr_NoElement(FILE *filein, int *&arr){
     arr = new int[n];
     if (arr==NULL){
         std::cout<<"Get error when declare the array"<<std::endl;
+        error_flag = 1;
     }
+    if (error_flag == 1)
+        return 0;
+    int i = 0;
     rewind(filein);
     int i = 0;
     while (!feof(filein))
